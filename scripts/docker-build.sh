8c6636cf5eae8ed5f3fcad4d1577b99d1d3b55a9
Xiang Zhu <zhuxiang1212@hotmail.com>
2020-12-16 19:26:38 +0800

    test

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -e
 
 fn-build() {
-    local registry = $1
-    local image_name = $2
-    local current_tag = "$(cat ./current-tag | tr -d '\n')"
+    local registry=$1
+    local image_name=$2
+    local current_tag="$(cat ./current-tag | tr -d '\n')"
     docker build -f /.Dockerfile --tag $registry/$image_name:$current_tag --tag $registry/$image_name:latest .
 }
 
